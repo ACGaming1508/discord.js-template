4e2818ba85e4c748b4dadde43ec82b67724c9846
ACGaming <88760089+ACGaming1508@users.noreply.github.com>
2022-02-26 21:46:12 +0700

    Update slashcommand.js

--- src/handlers/structures/slashcommand.js
+++ src/handlers/structures/slashcommand.js
@@ -8,19 +8,17 @@ module.exports = (client) => {
 	client.slashCommands = new Discord.Collection();
 	const SlashCommands = [];
 	fs.readdirSync('./commands').forEach(folder => {
-		if (folder !== 'Owner') {
-			const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(d => d.endsWith('.js') && typeof c.execute === 'function');
-			for (const file of commandFiles) {
-				const command = require(`../../commands/${folder}/${file}`);
-				client.slashCommands.set(command.data.name, command);
-				const s = {
-					'name': command.data.name,
-					'description': command.data.description,
-					'options': command.data.options,
-					'default_permission': command.data.default_permission,
-				};
-				SlashCommands.push(s);
-			}
+		const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(d => d.endsWith('.js') && typeof c.execute === 'function');
+		for (const file of commandFiles) {
+			const command = require(`../../commands/${folder}/${file}`);
+			client.slashCommands.set(command.data.name, command);
+			const s = {
+				'name': command.data.name,
+				'description': command.data.description,
+				'options': command.data.options,
+				'default_permission': command.data.default_permission,
+			};
+			SlashCommands.push(s);
 		}
 	});
 	const rest = new REST({ version: '9' }).setToken(config.ClientConfig.token);
